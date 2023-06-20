711f3956d3f4acdb1ecb5f29768a2d22b0dd29ca
Carlos7979 <carloscorredorwg@gmail.com>
2023-06-20 15:57:37 -0400

    add css to add product icons in Card component

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -10,18 +10,18 @@ export default {
     props: {
         product: Object,
         index: Number,
-		detail: {
-			type: Boolean,
-			default: false
-		},
-		quantity: {
-			type: Number,
-			default: 0
-		}
+        detail: {
+            type: Boolean,
+            default: false
+        },
+        quantity: {
+            type: Number,
+            default: 0
+        }
     },
     data() {
         return {
-            fav: false,
+            fav: false
         }
     },
     methods: {
@@ -29,20 +29,23 @@ export default {
             this.fav = !this.fav
         },
         addQuantity() {
-			this.$emit('quantity', [this.quantity + 1, this.product.id - 1 ])
+            this.$emit('quantity', [this.quantity + 1, this.product.id - 1])
         },
         removeQuantity() {
             if (this.quantity > 0) this.$emit('quantity', [this.quantity - 1, this.product.id - 1])
         },
+		emptyProduct() {
+			if (this.quantity > 0) this.$emit('quantity', [0, this.product.id - 1])
+		},
         handleShowInfo() {
-			if (this.detail) return
+            if (this.detail) return
             this.$emit('showInfo', ['showInfo', this.index])
         }
     }
 }
 </script>
 <template>
-    <div v-show="Object.keys(product).length > 0" :class="detail ? 'card detail': 'card'">
+    <div v-show="Object.keys(product).length > 0" :class="detail ? 'card detail' : 'card'">
         <div class="heart" @click="toggleFav">
             <div v-show="!fav"><Heart /></div>
             <div v-show="fav" class="fav"><HeartSolid /></div>
@@ -51,28 +54,34 @@ export default {
             :src="product.img"
             :class="detail ? 'card-img-top' : 'card-img-top card-image'"
             :alt="product.title"
-            :height="!detail ? '200': ''"
+            :height="!detail ? '200' : ''"
             @click="handleShowInfo"
         />
         <div class="card-body">
             <h5 class="card-title"></h5>
             <p class="card-text">
-                {{detail ? product.largeDescription : product.shortDescription }}
+                {{ detail ? product.largeDescription : product.shortDescription }}
             </p>
         </div>
         <ul class="list-group list-group-flush">
             <li class="list-group-item">Precio: {{ product.price }} $</li>
         </ul>
         <div class="card-body">
-            <span class="card-link">Agregar al carrito</span>
+            <div class="cart-header">
+                <span class="card-link">Agregar al carrito</span>
+				<span>Vaciar</span>
+            </div>
             <div class="add-cart-icons">
-                <div class="click-cart" @click="addQuantity">
-                    <div :class="quantity ? 'added-cart' : ''"><CartPlusIcon /></div>
+                <div class="add-cart">
+                    <div :class="quantity ? 'click-cart' : 'disable'" @click="removeQuantity">
+                        <div class="cart-element"><CartMinusIcon /></div>
+                    </div>
+                    <div class="quantity cart-element">{{ quantity }}</div>
+                    <div class="click-cart" @click="addQuantity">
+                        <div :class="quantity ? 'added-cart cart-element' : 'cart-element'"><CartPlusIcon /></div>
+                    </div>
                 </div>
-                <div class="quantity">{{ quantity }}</div>
-                <div :class="quantity ? 'click-cart' : 'disable'" @click="removeQuantity">
-                    <div :class="quantity ? 'remove-cart' : ''"><CartMinusIcon /></div>
-                </div>
+                <div @click="emptyProduct" :class="quantity ? 'fav click-cart' : 'disable'"><Trash /></div>
             </div>
         </div>
     </div>
@@ -82,12 +91,12 @@ export default {
 .card {
     margin: 5px;
     position: relative;
-	width: 18rem;
+    width: 18rem;
 }
 
 .detail {
-	min-width: 36rem;
-	height: auto;
+    min-width: 36rem;
+    height: auto;
 }
 
 .card-image {
@@ -120,9 +129,15 @@ export default {
 }
 
 .add-cart-icons {
-	display: flex;
-	justify-content: space-between;
+    display: flex;
+    justify-content: space-between;
 }
+
+.add-cart {
+	display: flex;
+	width: 50%;
+}
+
 .click-cart:active {
     position: relative;
     top: 1px;
@@ -140,4 +155,20 @@ export default {
 .disable {
     color: grey;
 }
+
+.cart-header {
+	display: flex;
+	justify-content: space-between;
+}
+
+.cart-element {
+    width: 50%;
+    text-align: center;
+}
+
+.quantity {
+	text-align: center;
+	width: 100%;
+}
+
 </style>
