0771481098bb4e5bec39222b47f6f5c2d98e2faa
Carlos7979 <carloscorredorwg@gmail.com>
2023-07-22 11:26:53 -0400

    add github config fot deploying

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,16 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+    const dev = mode === 'development'
+
+    return {
+        base: dev ? '/' : '/vue-project/',
+        plugins: [vue()],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url))
+            }
+        }
     }
-  }
 })
