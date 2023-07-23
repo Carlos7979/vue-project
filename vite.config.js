aaae0fad7f8bdb17a383f98308f76c6c07adc572
Carlos7979 <carloscorredorwg@gmail.com>
2023-07-22 21:11:55 -0400

    add rewrite plugin to vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import pluginRewriteAll from 'vite-plugin-rewrite-all'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -9,7 +10,7 @@ export default defineConfig(({ mode }) => {
 
     return {
         base: dev ? '/' : '/vue-project/',
-        plugins: [vue()],
+        plugins: [vue(), pluginRewriteAll()],
         resolve: {
             alias: {
                 '@': fileURLToPath(new URL('./src', import.meta.url))
